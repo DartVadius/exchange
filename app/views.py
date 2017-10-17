3cc74c35dfaa65a0829d80f7be637091b956ee3b
DartVadius <vad261275@gmail.com>
2017-10-17 17:20:03 +0300

    exchange

--- app/views.py
+++ app/views.py
@@ -1,12 +1,24 @@
-from flask import render_template, flash, redirect, request, url_for, session, jsonify, Flask
+from flask import render_template, flash, redirect, request, url_for, session
+
 from app import app
 from app import forms
-from app.dbmodels import Book, Currencies, StockExchanges, ExchangeHistory, ExchangeRates
 from app.database import db_session
+from app.dbmodels import Book, StockExchanges, ExchangeRates, Currencies
 
 
-@app.route('/stock', methods=['GET', 'POST'])
-def stock():
+@app.route('/stock/<int:stock_id>', methods=['GET'])
+def stock(stock_id):
+    # stock_id = request.form['id']
+    exchange_rates = db_session.query(ExchangeRates, StockExchanges).filter_by(stock_exchange_id=stock_id).join(
+        StockExchanges).add_columns(StockExchanges.name).join(
+        Currencies, ExchangeRates.compare_currency_id == Currencies.id).add_columns(Currencies.name).all()
+    for name in exchange_rates:
+        print(name.name)
+    return render_template("stock_view/stock_exchange.html", title='Stocks', rates=exchange_rates)
+
+
+@app.route('/stocks', methods=['GET', 'POST'])
+def stocks():
     form = forms.StockForm(request.form)
     stocks = db_session.query(StockExchanges).all()
     if request.method == 'POST' and form.validate():
@@ -17,8 +29,9 @@ def stock():
         exchange_stock = StockExchanges(name.lower(), url, api_key, api_secret)
         db_session.add(exchange_stock)
         db_session.commit()
-        return redirect(url_for('stock'))
-    return render_template("stock.html", title='Stocks', form=form, stocks=stocks)
+        return redirect(url_for('stocks'))
+    return render_template("stocks.html", title='Stocks', form=form, stocks=stocks)
+
 
 @app.route('/books')
 def index():
