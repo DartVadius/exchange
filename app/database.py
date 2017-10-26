9edfed537df95f9ef7189d49c5a7a35752e6196f
DartVadius <vad261275@gmail.com>
2017-10-26 14:43:05 +0300

    coins

--- app/database.py
+++ app/database.py
@@ -4,7 +4,14 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-engine = create_engine('mysql+pymysql://root:@localhost/mypy?charset=utf8', convert_unicode=True)
+
+dev = False
+if dev:
+    connect = 'mysql+pymysql://root:1111@localhost/mypy?charset=utf8'
+else:
+    connect = 'mysql+pymysql://coins:VfytnrJ@localhost/coins?charset=utf8'
+
+engine = create_engine(connect)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
